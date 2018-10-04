fedce4e3e5f1bdac917ba9680784a20f5465dc74
mevend <43105695+mevend@users.noreply.github.com>
2018-10-04 14:11:01 -0400

    Assignment3 (#7)
    * Initial commit for creating transaction
    
    * Merge to master (#1) (#8)
    
    Assignemt 2 cloud
    
    * Updated the transaction flow to persist the transaction and removed the hibernate validation checks on the transaction
    
    * Added the initial update functionality to check if the user owns the transaction and then proceed with the update
    
    * Added UUID for the transaction Id and updated the create, update and delete api calls

--- webapp/spring-login-master/src/main/java/com/cloud/repository/TransactionRepository.java
+++ webapp/spring-login-master/src/main/java/com/cloud/repository/TransactionRepository.java
@@ -1,15 +1,17 @@
 package com.cloud.repository;
 
 import java.util.List;
+import java.util.UUID;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cloud.model.Transaction;
 
 @Repository("transactionRepository")
-public interface TransactionRepository extends CrudRepository<Transaction, Integer> {
+public interface TransactionRepository extends CrudRepository<Transaction, UUID> {
 
-	Transaction findByTransactionId(int id);
+	Transaction findByTransactionId(UUID id);
 
 	List<Transaction> findByUserId(int userid);
 
