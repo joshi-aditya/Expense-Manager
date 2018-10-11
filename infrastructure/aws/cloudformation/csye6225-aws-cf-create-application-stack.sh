c84127ed6345d08be5c7aba50117a739e06d53ea
sskawitkar <43149937+sskawitkar@users.noreply.github.com>
2018-10-11 17:18:32 -0400

    Assignment4 (#5)
    * added filters and updated rds group
    
    * created dyanmoDB table
    
    * pulled from neha
    
    * pulled from neha
    
    * RDS instance and subnet
    
    * Delete application.properties

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -4,6 +4,7 @@ read -p 'Enter stack name: ' STACK_NAME
 vpc_id=$(aws ec2 describe-vpcs --filters "Name=isDefault,Values=false" --query "Vpcs[*].VpcId" --output text)
 echo $vpc_id
 subnet_id=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=${vpc_id}" "Name=mapPublicIpOnLaunch,Values=true" "Name=availabilityZone,Values=us-east-1a" --query 'Subnets[*].SubnetId'  --output text)
+subnet_id2=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=${vpc_id}" "Name=mapPublicIpOnLaunch,Values=true" "Name=availabilityZone,Values=us-east-1b" --query 'Subnets[*].SubnetId'  --output text)
 
 echo $subnet_id
 
@@ -15,7 +16,7 @@ echo "Creating stack.."
 
 STACK_ID=$(\aws cloudformation create-stack --stack-name ${STACK_NAME} \
 	--template-body file://csye6225-aws-cf-application.json\
-	--parameters ParameterKey=StackName,ParameterValue=${STACK_NAME} ParameterKey=VpcId,ParameterValue=${vpc_id} ParameterKey=SubnetId,ParameterValue=${subnet_id} ParameterKey=BucketName,ParameterValue=${bucket_name} \
+	--parameters ParameterKey=StackName,ParameterValue=${STACK_NAME} ParameterKey=VpcId,ParameterValue=${vpc_id} ParameterKey=SubnetId,ParameterValue=${subnet_id} ParameterKey=SubnetId2,ParameterValue=${subnet_id2} ParameterKey=BucketName,ParameterValue=${bucket_name} \
    | jq -r .StackId \
 )
 	
