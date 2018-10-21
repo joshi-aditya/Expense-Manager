91dfd344ff7f8d2f3ed83ce26960f21cf1d6706f
sskawitkar <43149937+sskawitkar@users.noreply.github.com>
2018-10-21 00:13:59 -0400

    Assignment5 (#3)
     Added policies and bucket and User roles,profiles and policies created

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
@@ -4,6 +4,10 @@ echo $STACK_NAME
 export TRAVIS_UPLOAD_TO_S3_POLICY_NAME=Travis-Upload-To-S3
 export TRAVIS_CODE_DEPLOY_POLICY_NAME=Travis-Code-Deploy
 export CODE_DEPLOY_EC2_S3_NAME=CodeDeploy-EC2-S3
+export CODE_DEPLOY_EC2_SERVICE_ROLE=CodeDeployEC2ServiceRole
+export CODE_DEPLOY_SERVICE_ROLE=CodeDeployServiceRole
+export CODE_DEPLOY_EC2_SERVICEROLE_SERVICE=ec2.amazonaws.com
+export CODE_DEPLOY_SERVICEROLE_SERVICE=codedeploy.amazonaws.com
 export account_id=$(aws sts get-caller-identity --query "Account" --output text)
 export region=us-east-1
 export application_name=csye6225-webapp
@@ -12,13 +16,12 @@ export resourse2="arn:aws:codedeploy:"$region":"$account_id":application:"$appli
 export resourse3="arn:aws:codedeploy:"$region":"$account_id":deploymentconfig:CodeDeployDefault.OneAtATime"
 export resourse4="arn:aws:codedeploy:"$region":"$account_id":deploymentconfig:CodeDeployDefault.HalfAtATime"
 export resourse5="arn:aws:codedeploy:"$region":"$account_id":deploymentconfig:CodeDeployDefault.AllAtOnce"
-
 domain_name=$(aws route53 list-hosted-zones --query 'HostedZones[0].Name' --output text)
 bucket_name="code-deploy."$domain_name"csye6225.com"
 
 STACK_ID=$(\aws cloudformation create-stack --stack-name ${STACK_NAME} \
 --template-body file://csye6225-cf-cicd.json \
---parameters ParameterKey=Resource1,ParameterValue=$resourse1 ParameterKey=Resource2,ParameterValue=$resourse2 ParameterKey=Resource3,ParameterValue=$resourse3 ParameterKey=Resource4,ParameterValue=$resourse4 ParameterKey=Resource5,ParameterValue=$resourse5 ParameterKey=TravisUploadToS3PolicyName,ParameterValue=$TRAVIS_UPLOAD_TO_S3_POLICY_NAME ParameterKey=TravisCDPolicyName,ParameterValue=$TRAVIS_CODE_DEPLOY_POLICY_NAME ParameterKey=CodeDeployEC2S3Name,ParameterValue=$CODE_DEPLOY_EC2_S3_NAME ParameterKey=BucketName,ParameterValue=${bucket_name} \
+--parameters ParameterKey=Resource1,ParameterValue=$resourse1 ParameterKey=Resource2,ParameterValue=$resourse2 ParameterKey=Resource3,ParameterValue=$resourse3 ParameterKey=Resource4,ParameterValue=$resourse4 ParameterKey=Resource5,ParameterValue=$resourse5 ParameterKey=TravisUploadToS3PolicyName,ParameterValue=$TRAVIS_UPLOAD_TO_S3_POLICY_NAME ParameterKey=TravisCDPolicyName,ParameterValue=$TRAVIS_CODE_DEPLOY_POLICY_NAME ParameterKey=CodeDeployEC2S3Name,ParameterValue=$CODE_DEPLOY_EC2_S3_NAME ParameterKey=CodeDeployEC2ServiceRoleService,ParameterValue=$CODE_DEPLOY_EC2_SERVICEROLE_SERVICE ParameterKey=CodeDeployServiceRoleService,ParameterValue=$CODE_DEPLOY_SERVICEROLE_SERVICE ParameterKey=CodeDeployEC2ServiceRoleName,ParameterValue=$CODE_DEPLOY_EC2_SERVICE_ROLE ParameterKey=CodeDeployServiceRoleName,ParameterValue=$CODE_DEPLOY_SERVICE_ROLE ParameterKey=BucketName,ParameterValue=${bucket_name} \
 --capabilities CAPABILITY_IAM \
 --capabilities CAPABILITY_NAMED_IAM \
 | jq -r .StackId \
@@ -26,4 +29,6 @@ STACK_ID=$(\aws cloudformation create-stack --stack-name ${STACK_NAME} \
 
 echo "Waiting on ${STACK_ID} create completion.."
 aws cloudformation wait stack-create-complete --stack-name ${STACK_ID}
-echo "Policies created!"
+echo "User policies created!"
+echo "User roles and profiles created !"
+echo "S3 Bucket created !"
