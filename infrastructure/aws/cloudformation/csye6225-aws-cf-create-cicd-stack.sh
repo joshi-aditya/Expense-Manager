3e0497b12f0b792cad9e06b692bf6f2d3f1d7d55
nbhangale <43249872+nbhangale@users.noreply.github.com>
2018-10-23 10:33:23 -0400

    Assignment5 (#6)
    * Added application code deploy and terminaion of cicd

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
@@ -10,7 +10,7 @@ export CODE_DEPLOY_EC2_SERVICEROLE_SERVICE=ec2.amazonaws.com
 export CODE_DEPLOY_SERVICEROLE_SERVICE=codedeploy.amazonaws.com
 export account_id=$(aws sts get-caller-identity --query "Account" --output text)
 export region=us-east-1
-export application_name=csye6225-webapp
+export application_name=csye6225-fall2018
 export resourse1="arn:aws:iam::"$account_id":user/travis-ci"
 export resourse2="arn:aws:codedeploy:"$region":"$account_id":application:"$application_name
 export resourse3="arn:aws:codedeploy:"$region":"$account_id":deploymentconfig:CodeDeployDefault.OneAtATime"
@@ -21,7 +21,7 @@ bucket_name="code-deploy."$domain_name"csye6225.com"
 
 STACK_ID=$(\aws cloudformation create-stack --stack-name ${STACK_NAME} \
 --template-body file://csye6225-cf-cicd.json \
---parameters ParameterKey=Resource1,ParameterValue=$resourse1 ParameterKey=Resource2,ParameterValue=$resourse2 ParameterKey=Resource3,ParameterValue=$resourse3 ParameterKey=Resource4,ParameterValue=$resourse4 ParameterKey=Resource5,ParameterValue=$resourse5 ParameterKey=TravisUploadToS3PolicyName,ParameterValue=$TRAVIS_UPLOAD_TO_S3_POLICY_NAME ParameterKey=TravisCDPolicyName,ParameterValue=$TRAVIS_CODE_DEPLOY_POLICY_NAME ParameterKey=CodeDeployEC2S3Name,ParameterValue=$CODE_DEPLOY_EC2_S3_NAME ParameterKey=CodeDeployEC2ServiceRoleService,ParameterValue=$CODE_DEPLOY_EC2_SERVICEROLE_SERVICE ParameterKey=CodeDeployServiceRoleService,ParameterValue=$CODE_DEPLOY_SERVICEROLE_SERVICE ParameterKey=CodeDeployEC2ServiceRoleName,ParameterValue=$CODE_DEPLOY_EC2_SERVICE_ROLE ParameterKey=CodeDeployServiceRoleName,ParameterValue=$CODE_DEPLOY_SERVICE_ROLE ParameterKey=BucketName,ParameterValue=${bucket_name} \
+--parameters ParameterKey=Resource1,ParameterValue=$resourse1 ParameterKey=Resource2,ParameterValue=$resourse2 ParameterKey=Resource3,ParameterValue=$resourse3 ParameterKey=Resource4,ParameterValue=$resourse4 ParameterKey=Resource5,ParameterValue=$resourse5 ParameterKey=TravisUploadToS3PolicyName,ParameterValue=$TRAVIS_UPLOAD_TO_S3_POLICY_NAME ParameterKey=TravisCDPolicyName,ParameterValue=$TRAVIS_CODE_DEPLOY_POLICY_NAME ParameterKey=CodeDeployEC2S3Name,ParameterValue=$CODE_DEPLOY_EC2_S3_NAME ParameterKey=CodeDeployEC2ServiceRoleService,ParameterValue=$CODE_DEPLOY_EC2_SERVICEROLE_SERVICE ParameterKey=CodeDeployServiceRoleService,ParameterValue=$CODE_DEPLOY_SERVICEROLE_SERVICE ParameterKey=CodeDeployEC2ServiceRoleName,ParameterValue=$CODE_DEPLOY_EC2_SERVICE_ROLE ParameterKey=CodeDeployServiceRoleName,ParameterValue=$CODE_DEPLOY_SERVICE_ROLE ParameterKey=ApplicationName,ParameterValue=${application_name} ParameterKey=BucketName,ParameterValue=${bucket_name} \
 --capabilities CAPABILITY_IAM \
 --capabilities CAPABILITY_NAMED_IAM \
 | jq -r .StackId \
