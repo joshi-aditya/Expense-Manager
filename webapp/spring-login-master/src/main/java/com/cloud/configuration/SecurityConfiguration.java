aeee852b35d0e13a97a6441a00f23b2ded395c3f
sskawitkar <43149937+sskawitkar@users.noreply.github.com>
2018-10-23 22:00:09 -0400

    Changed in AfterInstall.sh (#8)
    * Added policies and bucket
    
    * User roles,profiles and policies created
    
    * travis,appspec and codedeploy scripts added
    
    * Target path changed with war dependency
    
    * path changed for .war
    
    * mvnw path changed and clean and build
    
    * .properties modified
    
    * .properties modified
    
    * test cases commented
    
    * changed travis file
    
    * changed travis file
    
    * changed travis file
    
    * exposed api to test
    
    * exposed api to test
    
    * war file name changed in afterinstall.sh

--- webapp/spring-login-master/src/main/java/com/cloud/configuration/SecurityConfiguration.java
+++ webapp/spring-login-master/src/main/java/com/cloud/configuration/SecurityConfiguration.java
@@ -39,6 +39,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/time").permitAll()
         		.antMatchers("/user/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
