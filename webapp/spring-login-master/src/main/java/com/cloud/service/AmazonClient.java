11fc93ee82fb75f996050d1e64f8204578674451
aditya-joshi-neu <43189825+aditya-joshi-neu@users.noreply.github.com>
2018-11-05 19:55:45 -0500

    Triggering build

--- webapp/spring-login-master/src/main/java/com/cloud/service/AmazonClient.java
+++ webapp/spring-login-master/src/main/java/com/cloud/service/AmazonClient.java
@@ -34,7 +34,7 @@ public class AmazonClient implements BaseClient{
 	@Value("${amazonProperties.endpointUrl}")
 	private String endpointUrl;
 	//@Value("${spring.bucket.name}")
-	private String bucketName = "";
+	private String bucketName;
 	//@Value("${amazonProperties.bucketName}")
 	//private String bucketName;
 
